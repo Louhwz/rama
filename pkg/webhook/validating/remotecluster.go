81b37c7995a9a7f501c66f1c7894deaaa8104c9b
Louhwz <872188659@qq.com>
2021-08-10 10:22:23 +0800

    feat: remote cluster webhook

--- pkg/webhook/validating/remotecluster.go
+++ pkg/webhook/validating/remotecluster.go
@@ -1,3 +1,57 @@
 package validating
 
+import (
+	"context"
+	"net/http"
+	"regexp"
 
+	ramav1 "github.com/oecp/rama/pkg/apis/networking/v1"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+var (
+	r                = regexp.MustCompile(`^(https?://)[\w-]+(\.[\w-]+)+:\d{1,5}$`)
+	remoteClusterGVK = gvkConverter(ramav1.SchemeGroupVersion.WithKind("RemoteCluster"))
+)
+
+func init() {
+	createHandlers[remoteClusterGVK] = RCCreateValidation
+	updateHandlers[remoteClusterGVK] = RCUpdateValidation
+	deleteHandlers[remoteClusterGVK] = RCDeleteValidation
+}
+
+func RCCreateValidation(ctx context.Context, req *admission.Request, handler *Handler) admission.Response {
+	rc := &ramav1.RemoteCluster{}
+	err := handler.Decoder.Decode(*req, rc)
+	if err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+	return validate(ctx, rc)
+}
+
+func RCUpdateValidation(ctx context.Context, req *admission.Request, handler *Handler) admission.Response {
+	var (
+		err   error
+		newRC *ramav1.RemoteCluster
+	)
+	if err = handler.Decoder.DecodeRaw(req.Object, newRC); err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+	return validate(ctx, newRC)
+}
+
+func RCDeleteValidation(ctx context.Context, req *admission.Request, handler *Handler) admission.Response {
+	return admission.Allowed("validation pass")
+}
+
+func validate(ctx context.Context, rc *ramav1.RemoteCluster) admission.Response {
+	connConfig := rc.Spec.ConnConfig
+	if connConfig.Endpoint == "" || connConfig.CABundle == nil || connConfig.ClientKey == nil || connConfig.ClientCert == nil {
+		return admission.Denied("empty connection config, please check.")
+	}
+
+	if !r.Match([]byte(connConfig.Endpoint)) {
+		return admission.Denied("endpoint format: https://server:address")
+	}
+	return admission.Allowed("validation pass")
+}
