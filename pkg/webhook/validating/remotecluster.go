c1c5e167426b4818a96808d16913a6d82c243254
Louhwz <872188659@qq.com>
2021-08-11 10:29:26 +0800

    feat: add multi cluster webhook

--- pkg/webhook/validating/remotecluster.go
+++ pkg/webhook/validating/remotecluster.go
@@ -2,15 +2,21 @@ package validating
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"regexp"
+	"sync"
 
 	ramav1 "github.com/oecp/rama/pkg/apis/networking/v1"
+	"github.com/oecp/rama/pkg/utils"
+	"k8s.io/apimachinery/pkg/labels"
+	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 var (
 	r                = regexp.MustCompile(`^(https?://)[\w-]+(\.[\w-]+)+:\d{1,5}$`)
+	mu               sync.Mutex
 	remoteClusterGVK = gvkConverter(ramav1.SchemeGroupVersion.WithKind("RemoteCluster"))
 )
 
@@ -45,13 +51,35 @@ func RCDeleteValidation(ctx context.Context, req *admission.Request, handler *Ha
 }
 
 func validate(ctx context.Context, rc *ramav1.RemoteCluster) admission.Response {
+	mu.Lock()
+	defer mu.Unlock()
+
 	connConfig := rc.Spec.ConnConfig
 	if connConfig.Endpoint == "" || connConfig.CABundle == nil || connConfig.ClientKey == nil || connConfig.ClientCert == nil {
 		return admission.Denied("empty connection config, please check.")
 	}
-
 	if !r.Match([]byte(connConfig.Endpoint)) {
-		return admission.Denied("endpoint format: https://server:address")
+		return admission.Denied("endpoint format: https://server:address, please check")
+	}
+
+	cfg, err := utils.BuildClusterConfig(rc)
+	if err != nil {
+		return admission.Denied(fmt.Sprintf("Can't build connection to remote cluster, maybe wrong config. Err=%v", err.Error()))
+	}
+	coreClient := v1.NewForConfigOrDie(cfg)
+	uuid, err := utils.GetUUID(coreClient)
+	if err != nil {
+		return admission.Denied(fmt.Sprintf("Can't get uuid. Err=%v", err))
+	}
+
+	rcs, err := RCLister.List(labels.NewSelector())
+	if err != nil {
+		return admission.Denied(fmt.Sprintf("Can't list remote cluster. Err=%v", err))
+	}
+	for _, rc := range rcs {
+		if uuid == rc.Status.UUID {
+			return admission.Denied("Duplicate cluster configuration")
+		}
 	}
 	return admission.Allowed("validation pass")
 }
